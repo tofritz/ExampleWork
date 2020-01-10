fd667cf7fd40e2f620fd182cd29b1252c97bb0a8
tofritz <tfritz@smu.edu>
2020-01-09 19:50:39 -0600

    getting closer

--- practice-problems/smorse/smorse-2.py
+++ practice-problems/smorse/smorse-2.py
@@ -11,6 +11,11 @@ def get_letter(seq):
         if seq == morse:
             return letter
 
+
+def decode(query):
+    return([word for (word, morse) in wordsDict.items() if morse == query])
+
+
 # brute-force replace characters as encountered
 
 # def smalpha_1(smorse: str):
@@ -30,10 +35,52 @@ def get_letter(seq):
 
 # pop out of list sorted by morse length after substitution
 
-def smalpha_2(smorse: str):
-    toReplace = list(morseDict.items())
-    toReplace.sort(reverse=True key=lambda x: len(x[0]))
-    return toReplace
+# def smalpha_2(smorse: str):
+#     toReplace = list(morseDict.items())
+#     toReplace.sort(reverse=True, key=lambda x: len(x[0]))
+#     for (morse, letter) in toReplace:
+#         smorse = smorse.replace(morse, letter, 1)
+#         toReplace.pop(0)
+#     return(smorse)
 
 
-print(smalpha_2(test))
+# print(smalpha_2(test))
+
+# count frequency of morse in smorse, replace low > high freq.
+
+morseFreq = {morse: 0 for morse in morseDict.keys()}  # counter
+
+
+# def reset():
+#     morseFreq = dict.fromkeys(morseFreq, 0)
+
+def reset():
+    for morse, freq in morseFreq.items():
+        morseFreq[morse] = 0
+
+
+def count(smorse: str):
+    for morse, freq in morseFreq.items():
+        morseFreq[morse] = smorse.count(morse)
+
+
+# def rank(rankFreq):
+#     rankFreq = list(morseFreq.items())
+#     rankFreq.sort(key=lambda x: x[1])
+#     return rankFreq
+
+
+def smalpha_freq(smorse: str):
+    counter = 26
+    count(smorse)
+    rankFreq = list(morseFreq.items())
+    rankFreq.sort(key=lambda x: x[1])
+    while counter > 0:
+        smorse = smorse.replace(rankFreq[0][0], morseDict[rankFreq[0][0]], 1)
+        del morseFreq[rankFreq[0][0]]
+        del rankFreq[0]
+        counter -= 1
+    return smorse
+
+
+print(smalpha_freq(test))
