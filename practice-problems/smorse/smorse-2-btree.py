8af3d01590dba7aa149a4bdc98ecacab53875da1
tyler <tyofritz@gmail.com>
2020-01-11 23:24:27 -0600

    solved with recursive generator func.

--- practice-problems/smorse/smorse-2-btree.py
+++ practice-problems/smorse/smorse-2-btree.py
@@ -1,31 +1,39 @@
 from string import ascii_lowercase
+import timeit
 
 morse_alphabet = ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..".split()
-char_to_morse = dict(zip(ascii_lowercase, morse_alphabet))
 morse_to_char = dict(zip(morse_alphabet, ascii_lowercase))
+char_to_morse = dict(zip(ascii_lowercase, morse_alphabet))
 
 
-def smalpha(morse: str, found=""):
-    if morse == "" and set(found) == set(ascii_lowercase):
-        return(''.join(found))
+def recurse(smorse, remaining = set(morse_alphabet), found = []):
+    if not smorse:
+        yield found
+        return
+    
+    for morse in remaining:
+        if morse == smorse[:len(morse)]:
+            found.append(morse)
+            trim = remaining.copy()
+            trim.remove(morse)
+            yield from recurse(smorse[len(morse):], trim, found)
+            found.pop()
 
-    for letter in {letter for letter in ascii_lowercase if letter not in found}:
-        if not morse.startswith(char_to_morse[letter]):
-            continue
-        _found = found + letter
-        _morse = morse[len(char_to_morse[letter]):]
-        try:
-            return smalpha(_morse, _found)
-        except:
-            raise SomethingWentWrong
-    else:
-        raise SomethingWentWrong
+def smalpha(smorse):
+    for found in recurse(smorse):
+        return ''.join([morse_to_char[morse] for morse in found])
 
+def encode(string: str):
+    return ''.join(char_to_morse[char] for char in string)
 
-class SomethingWentWrong(Exception):
-    print("Something went horribly wrong.")
-    pass
+def check(input):
+    output = smalpha(input)
+    assert encode(output) == input
+    assert set(output) == set(ascii_lowercase)
+    return output
 
+def challenge():
+    check('.--...-.-.-.....-.--........----.-.-..---.---.--.--.-.-....-..-...-.---..--.----..')
+    return print('Challenge Passed!')
 
-test = '.--...-.-.-.....-.--........----.-.-..---.---.--.--.-.-....-..-...-.---..--.----..'
-print(smalpha(test))
+challenge()
